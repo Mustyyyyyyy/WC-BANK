735a837c08749b1bc314b7e5e0ca5cbb8b87828c
Mustyyyyyyy <alagbemustapha02@gmail.com>
2025-10-06 15:10:29 +0100

    w

--- Front/src/api.jsx
+++ Front/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://wc-2.onrender.com",
+  baseURL: process.env.REACT_APP_API_URL || "https://wc-2.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
