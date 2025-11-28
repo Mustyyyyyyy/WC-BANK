8b19a0e41e2b9c360ad35c804ba31111f04744eb
Mustyyyyyyy <alagbemustapha02@gmail.com>
2025-11-28 09:16:18 +0100

    W

--- Server/index.js
+++ Server/index.js
@@ -7,16 +7,22 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: ["https://wc-bank-d92y.vercel.app", "http://localhost:5173"],
-    methods: "GET,POST,PUT,DELETE,OPTIONS",
-    allowedHeaders: "Content-Type, Authorization",
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: [
+    "https://wc-bank.vercel.app", 
+    "http://localhost:5173"       
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true, 
+};
+
+app.use(cors(corsOptions));
+
+app.options("*", cors(corsOptions));
 
 const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
